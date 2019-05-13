1442af8150fd522f41a86c225a9f14841997716c
Groku <kevin.freminet@hotmail.fr>
2019-05-13 13:21:52 +0200

    Top Layout + Actor Layout

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,13 +1,14 @@
+
 #include "mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
         : QWidget(parent)
 {
     //Layout creation of the movies already seen
-    see_radiobutton = new QRadioButton("Vu",this);
+    see_check = new QCheckBox("Vu",this);
     historyseen_button = new QPushButton("Historique vues",this);
     alreadyseen_layout = new QHBoxLayout;
-    alreadyseen_layout->addWidget(see_radiobutton,1);
+    alreadyseen_layout->addWidget(see_check,1);
     alreadyseen_layout->addWidget(historyseen_button,2);
 
     //Layout creation at the bottom right
@@ -22,23 +23,56 @@ MainWindow::MainWindow(QWidget *parent)
     bot_layout->addWidget(moreinfo_text,3);
     bot_layout->addLayout(see_layout,1);
 
+    //Layout creation with actor & genre
+    actor_label = new QLabel("acteurs",this);
+    genre_label = new QLabel("Genres",this);
+    actor1_label = new QLabel("actor1",this);
+    actor2_label = new QLabel("actor2",this);
+    actor3_label = new QLabel("actor3",this);
+    genre1_label = new QLabel("genre1",this);
+    genre2_label = new QLabel("genre2",this);
+    genre3_label = new QLabel("genre3",this);
+    actor_layout = new QVBoxLayout;
+    genre_layout = new QVBoxLayout;
+    actor_genre_layout = new QHBoxLayout;
+    actor_layout->addWidget(actor_label);
+    actor_layout->addWidget(actor1_label);
+    //actor1_label->setStyleSheet("border: 1px solid black");
+    actor_layout->addWidget(actor2_label);
+    actor_layout->addWidget(actor3_label);
+    genre_layout->addWidget(genre_label);
+    genre_layout->addWidget(genre1_label);
+    genre_layout->addWidget(genre2_label);
+    genre_layout->addWidget(genre3_label);
+    actor_genre_layout->addLayout(actor_layout);
+    actor_genre_layout->addLayout(genre_layout);
+
     //Layout creation at the middle
     image_label = new QLabel(this);
-    actor_text = new QTextEdit(this);
     poster_layout = new QHBoxLayout;
     poster_layout->addWidget(image_label,1);
-    poster_layout->addWidget(actor_text,2);
-    synopsys_label = new QLabel(this);
+    poster_layout->addLayout(actor_genre_layout,1);
+    synopsys_label = new QLabel("Synopsys",this);
     synopsys_text = new QTextEdit(this);
     middle_layout = new QVBoxLayout;
-    middle_layout->addLayout(poster_layout,4);
-    middle_layout->addWidget(synopsys_label,0.5);
-    middle_layout->addWidget(synopsys_text,4);
+    middle_layout->addLayout(poster_layout,8);
+    middle_layout->addWidget(synopsys_label,1);
+    middle_layout->addWidget(synopsys_text,8);
+
+    //Layout creation ah the top
+    movie_list = new QComboBox(this);
+    search_button = new QPushButton("Recherche", this);
+    top_layout = new QHBoxLayout;
+    top_layout->addWidget(movie_list,8);
+    top_layout->addWidget(search_button,1);
+
+
 
     //We gather all the layout
     final_layout = new QVBoxLayout;
-    final_layout->addLayout(middle_layout,2);
-    final_layout->addLayout(bot_layout,1);
+    final_layout->addLayout(top_layout,1);
+    final_layout->addLayout(middle_layout,6);
+    final_layout->addLayout(bot_layout,3);
 
 
 
--- MainWindow.h
+++ MainWindow.h
@@ -9,6 +9,8 @@
 #include <QRadioButton>
 #include <QTextEdit>
 #include <QLabel>
+#include <QCheckBox>
+#include <QComboBox>
 
 class MainWindow : public QWidget
 {
@@ -25,27 +27,44 @@ private:
     QVBoxLayout* final_layout;
     QVBoxLayout* see_layout;
     QVBoxLayout* middle_layout;
+    QVBoxLayout* actor_layout;
+    QVBoxLayout* genre_layout;
 
     QHBoxLayout* alreadyseen_layout;
     QHBoxLayout* bot_layout;
     QHBoxLayout* poster_layout;
+    QHBoxLayout* top_layout;
+    QHBoxLayout* actor_genre_layout;
 
     //QPushButton creation
     QPushButton* historyseen_button;
     QPushButton* edit_button;
+    QPushButton* search_button;
 
-    //QRadioButton creation
-    QRadioButton* see_radiobutton;
+    //QCheckBox creation
+    QCheckBox* see_check;
 
     //QTextEdit creation
     QTextEdit* moreinfo_text;
     QTextEdit* actor_text;
     QTextEdit* synopsys_text;
 
-
     //QLabel creation
-    QLabel* image_label;
+    //Without border
     QLabel* synopsys_label;
+    QLabel* actor_label;
+    QLabel* genre_label;
+    //With border
+    QLabel* image_label;
+    QLabel* actor1_label;
+    QLabel* actor2_label;
+    QLabel* actor3_label;
+    QLabel* genre1_label;
+    QLabel* genre2_label;
+    QLabel* genre3_label;
+
+    //QComboBox creation
+    QComboBox* movie_list;
 
 };
 
